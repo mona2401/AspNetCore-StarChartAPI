74f5a3f2dc1558a1aa10b74fb2b241f524c056de
Monalisa Panda <51704971+mona2401@users.noreply.github.com>
2022-07-29 11:09:25 +0530

    MOdule 3

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int? OrbitedObjectId { get; set; }
+
+        [NotMapped]
+        public List<CelestialObject> Satellites;
+
+        public DateTime OrbitalPeriod { get; set; }
+    }
+}
